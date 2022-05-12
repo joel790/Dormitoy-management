45a7dbca031402afb892a9a521f4645fdf16fcb5
joel790 <eyuelkassahun3@gmail.com>
2022-05-12 23:35:39 +0300

    this is branch1 commit

--- proctorMananger.java
+++ proctorMananger.java
@@ -36,3 +36,54 @@ public class proctorMananger {
 	}
 
 }
+/**
+ * @author AKAM
+ * @version 1.0
+ * @created 12-May-2022 2:11:34 PM
+ */
+public class faculity {
+
+	public string faculity_name;
+	private int faculity_no;
+	public int noOfdepartment;
+	public department m_department;
+
+	public faculity(){
+
+	}
+
+	public void finalize() throws Throwable {
+
+	}
+
+	public void get_faculity(){
+
+	}
+
+}
+/**
+ * @author AKAM
+ * @version 1.0
+ * @created 12-May-2022 2:11:34 PM
+ */
+public class faculity {
+
+	public string faculity_name;
+	private int faculity_no;
+	public int noOfdepartment;
+	public department m_department;
+
+	public faculity(){
+
+	}
+
+	public void finalize() throws Throwable {
+
+	}
+
+	public void get_faculity(){
+
+	}
+
+}
+/*this is the two calsses*/
