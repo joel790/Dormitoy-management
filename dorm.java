6dbf22183c14420937fe6343c016657acd3da9f1
21g21e <gebremariamassega@gmail.com>
2022-05-13 23:30:02 +0300

    class proctor manager added

---
+++ dorm.java
@@ -0,0 +1,65 @@
+
+
+/**
+ * @author AKAM
+ * @version 1.0
+ * @created 12-May-2022 2:11:34 PM
+ */
+public class dorm {
+
+	public int block_no;
+	public int capacity;
+	public int dorm_no;
+	public int free_dorm;
+	public int no_student;
+	public material m_material;
+
+	public dorm(){
+
+	}
+
+	public void finalize() throws Throwable {
+
+	}
+
+	public void getDorm(){
+
+	}
+
+	public void recordDorm(){
+
+	}
+
+}
+
+
+/**
+ * @author AKAM
+ * @version 1.0
+ * @created 12-May-2022 2:11:34 PM
+ */
+public class material {
+
+	public int block_no;
+	public string blockName;
+	public int dormNo;
+	public string itemType;
+	public string quantity;
+
+	public material(){
+
+	}
+
+	public void finalize() throws Throwable {
+
+	}
+
+	public void checkStatus(){
+
+	}
+
+	public void getMaterial(){
+
+	}
+
+}
---
+++ proctorMananger.java
@@ -0,0 +1,61 @@
+
+
+
+public class proctorMananger {
+
+	public string address;
+	public int age;
+	public int blockNo;
+	public string campus;
+	public string ciity;
+	public string first_name;
+	public string last_name;
+	private int proctorManagerId;
+	public string sex;
+	public student m_student;
+	public building m_building;
+
+	public proctorMananger(){
+
+	}
+
+	public void finalize() throws Throwable {
+
+	}
+
+	public void addProctor(){
+
+	}
+
+	public void arrangeBuilding(){
+
+	}
+	
+
+/**
+ * @author AKAM
+ * @version 1.0
+ * @created 12-May-2022 2:11:34 PM
+ */
+public class faculity {
+
+	public string faculity_name;
+	private int faculity_no;
+	public int noOfdepartment;
+	public department m_department;
+
+	public faculity(){
+
+	}
+
+	public void finalize() throws Throwable {
+
+	}
+
+	public void get_faculity(){
+
+	}
+
+}
+
+}
