480793ae549334c1d9289f053b9535098b769365
betigirma <103247620+betigirma@users.noreply.github.com>
2022-05-13 14:31:14 +0300

    updated information

--- proctorMananger.java
+++ proctorMananger.java
@@ -2,7 +2,7 @@
 
 /**
  * @author AKAM
- * @version 1.00
+ * @version 1.0
  * @created 12-May-2022 2:11:34 PM
  */
 public class proctorMananger {
@@ -34,5 +34,32 @@ public class proctorMananger {
 	public void arrangeBuilding(){
 
 	}
+	
+
+/**
+ * @author AKAM
+ * @version 1.0
+ * @created 12-May-2022 2:11:34 PM
+ */
+public class faculity {
+
+	public string faculity_name;
+	private int faculity_no;
+	public int noOfdepartment;
+	public department m_department;
+
+	public faculity(){
+
+	}
+
+	public void finalize() throws Throwable {
+
+	}
+
+	public void get_faculity(){
+
+	}
+
+}
 
 }
